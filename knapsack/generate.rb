1c4b7476bd54a261eeaa854457717e30460c710a
Shohei Aoki <shoaok@gmail.com>
2015-07-26 20:01:24 +0900

    add genertaor

---
+++ knapsack/generate.rb
@@ -0,0 +1,15 @@
+require 'faker'
+require 'csv'
+
+
+CSV.open('contentes.csv','w') do |csv|
+  csv << ['name','weight','price']
+  (1..50).map do |i|
+    name = Faker::Commerce.product_name
+    weight = rand(1..10)
+    price =  rand(1..40) 
+    csv << [name,weight,price]
+  end
+end
+
+
